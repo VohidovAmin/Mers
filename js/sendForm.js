7970b1c26046dde0b28e539c30a1bb88da04cc5f
VohidovAmin <vohidovaminjon9@gmail.com>
2026-04-12 22:18:02 +0300

    Четвертый коммит

---
+++ js/sendForm.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('.form-test-drive');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data = {};
+
+    for (let { name, value } of form.elements) {
+        if (name) {
+            if (value.trim() === '') {
+                alert('Необходимо заполнить форму!')
+                return
+            } 
+            
+            data[name] = value;
+        }
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (response.status === 200 || response.status === 201) {
+                return response.json()
+            } else {
+                throw new Error(response.status)
+            }
+        })
+        .then(data => {
+            alert('Данные успешно отправлены!')
+            form.reset()
+        })
+        .catch(error => {
+            alert('Произошла ошибкаб, статуса ' + error.message)
+        })
+})
